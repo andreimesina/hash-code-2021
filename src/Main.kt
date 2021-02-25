bc7fb2bc595ade81f9e0a243413dcf94d1745612
tchrys <tanasescuchrys@gmail.com>
2021-02-25 21:49:06 +0200

    dsdg

--- src/Main.kt
+++ src/Main.kt
@@ -2,7 +2,7 @@ fun main() {
     val simulation = "a.txt".readInput()
 
     val incomingStreets: MutableMap<Int, List<Street>> = mutableMapOf()
-//    val carsWaitingAtInters: Muta
+    val carsWaitingAtSem: MutableMap<Int, MutableMap<Int, Int>> = mutableMapOf()
 
     (0 until simulation.intersectionsNumber).forEach { intersectionId ->
         val streets = simulation.streets.filter { street ->
@@ -20,7 +20,15 @@ fun main() {
         var time = 0
         car.streetsNames.forEach { s: String ->
             val street: Street = simulation.streets[simulation.streetNameToId[s]!!]
-
+            var mapForSem: MutableMap<Int, Int>? = null
+            if (carsWaitingAtSem.containsKey(street.id)) {
+                mapForSem = carsWaitingAtSem[street.id]
+            } else {
+                mapForSem = mutableMapOf()
+            }
+            mapForSem.set(time, mapForSem?.getOrDefault(time, 0)?.plus(1))
+            carsWaitingAtSem[street.endIntersectionId] = mapForSem
+            time += street.duration
         }
     }
 
